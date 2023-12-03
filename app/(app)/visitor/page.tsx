750223aa34fe03ecb13a25c4de64512e5d7d4c53
Elias Godson <eng.eliasgodson@gmail.com>
2023-12-03 10:01:55 +0300

    modifying some padding and margins on the form

--- app/(app)/visitor/page.tsx
+++ app/(app)/visitor/page.tsx
@@ -13,7 +13,7 @@ export default function IndexPage() {
       </div>
       
     </section>
-    <div className=" m-10 p-6 shadow-lg rounded-lg "><VisitorForm /></div>
+    <div className=" m-10 px-14 py-6 shadow-lg rounded-lg "><VisitorForm /></div>
     
    </>
   )
