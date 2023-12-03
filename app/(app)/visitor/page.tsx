6b2402a3635d357cf6932dd077b8ce19c571cfac
Elias Godson <eng.eliasgodson@gmail.com>
2023-12-03 09:33:20 +0300

    add div element to style visitors form

--- app/(app)/visitor/page.tsx
+++ app/(app)/visitor/page.tsx
@@ -2,7 +2,8 @@ import { VisitorForm } from "@/components/forms/VisitorForm";
 
 export default function IndexPage() {
   return (
-    <> <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    <> 
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex w-full flex-col items-center gap-2 justify-center text-center  ">
        
         <h2 className="font-heading text-3xl font-extrabold  leading-[1.1] sm:text-3xl md:text-4xl">
@@ -12,7 +13,8 @@ export default function IndexPage() {
       </div>
       
     </section>
-    <VisitorForm />
+    <div className=" m-10 p-6 shadow-lg rounded-lg "><VisitorForm /></div>
+    
    </>
   )
 }
