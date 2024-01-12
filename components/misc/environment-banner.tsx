799f74098d1508bbf929bc04aef7b536d57ef719
Fredy <fredgerman30@gmail.com>
2024-01-12 07:19:16 +0300

    feat : theme update

--- components/misc/environment-banner.tsx
+++ components/misc/environment-banner.tsx
@@ -2,7 +2,7 @@ import { env } from "@/env.mjs";
 
 export default function EnvironmentBanner() {
   return env.NEXT_PUBLIC_VERCEL_ENV !== "production" ? (
-    <div className="flex h-4 w-full items-center justify-center bg-violet-500 text-xs font-bold text-primary">
+    <div className="flex h-4 w-full items-center justify-center bg-destructive text-xs font-bold text-destructive-foreground">
       Development Environment
     </div>
   ) : null;
