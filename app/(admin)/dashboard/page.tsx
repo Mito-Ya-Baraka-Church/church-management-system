7bd55b6a0baa677c0d302e2fe2df9e09eb08f651
Fredy <fredgerman30@gmail.com>
2024-01-13 23:56:20 +0300

    feat (wip) : dashboard page ui

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,42 +1,19 @@
-import Link from "next/link"
+import Link from "next/link";
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import { siteConfig } from "@/config/site";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          Dashboard Page
         </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">AUTH</p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={
-            siteConfig.socials.find((social) => social.name === "twitter")
-              ?.url ?? "#"
-          }
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={
-            siteConfig.socials.find((social) => social.name === "github")
-              ?.url ?? "#"
-          }
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          This is the dashboard page. You can add your own content here.
+        </p>
       </div>
     </section>
-  )
+  );
 }
